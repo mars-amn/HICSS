b4dc4cededcbc9c5d0d776a7e9041cc4694955a9
fatma <fatma_s3@yahoo.com>
2019-02-14 16:33:33 -0800

    fa

--- app/src/main/java/alarish/hicss/hicss/MainActivity.java
+++ app/src/main/java/alarish/hicss/hicss/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
